a64574c6ee676de47d4afaf53369602543c914a5
jiparis <jiparis@gmail.com>
2011-05-02 01:34:39 +0200

    First commit

---
+++ src/jip/monome/serialosc/GridListener.java
@@ -0,0 +1,11 @@
+package jip.monome.serialosc;
+
+public interface GridListener {
+	/**
+	 * called when a button is pressed or released
+	 * @param x
+	 * @param y
+	 * @param state
+	 */
+	public void press(int x, int y, int state);
+}
---
+++ src/jip/monome/serialosc/TiltListener.java
@@ -0,0 +1,12 @@
+package jip.monome.serialosc;
+
+public interface TiltListener {
+	/**
+	 * called when a sensor changes
+	 * @param sensor
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public void tilt(int sensor, int x, int y, int z);
+}
